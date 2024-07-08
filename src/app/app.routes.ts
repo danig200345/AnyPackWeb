749dded248503d456418192fc88380485696cd93
danig200345 <126895966+danig200345@users.noreply.github.com>
2024-07-08 09:14:25 -0600

    new

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.component')
+  },
+  {
+    path: 'contacto',
+    loadComponent: () => import('./pages/contacto/contacto.component')
+  },
+
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: '**', redirectTo: 'home', pathMatch: 'full'
+  }
+];
--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,12 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, ReactiveFormsModule, CommonModule, HttpClientModule],
   templateUrl: './contacto.component.html',
-  styleUrl: './contacto.component.css'
+  styleUrls: ['./contacto.component.css']
 })
-export class ContactoComponent {
+export default class ContactoComponent implements OnInit {
+  contactForm: FormGroup;
+  loading = false;
+  sentMessage = false;
+  errorMessage = '';
+  private _success = new Subject<string>();
+  private _error = new Subject<string>();
 
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    this._success.subscribe(() => this.sentMessage = true);
+    this._success.pipe(debounceTime(5000)).subscribe(() => this.sentMessage = false);
+
+    this._error.subscribe(message => this.errorMessage = message);
+    this._error.pipe(debounceTime(10000)).subscribe(() => this.errorMessage = '');
+  }
+
+  onSubmit(): void {
+    if (this.contactForm.valid) {
+      this.loading = true;
+      this.sentMessage = false;
+      this.errorMessage = '';
+
+      this.http.post('http://localhost:3000/envio', this.contactForm.value)
+        .subscribe(
+          response => {
+            this.loading = false;
+            this._success.next('');
+            this.contactForm.reset();
+          },
+          error => {
+            this.loading = false;
+            this._error.next('Error al enviar el mensaje. Por favor, inténtalo de nuevo.');
+          }
+        );
+    } else {
+      this.contactForm.markAllAsTouched();
+      this._error.next('Por favor, completa todos los campos del formulario.');
+    }
+  }
+
+  email = 'redes.sociales@anypack.mx';
+
+  scrollTop() {
+    setTimeout(() => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    }, 50);  // Ajusta el tiempo de retraso si es necesario
+  }
 }
